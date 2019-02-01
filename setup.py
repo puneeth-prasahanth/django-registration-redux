9c0ad5ba2fe29be9bb3203874175de1fb272a0e5
Puneeth Prashanth Ganapuram <PuneethPrashanth.Ganapuram>
2019-02-01 17:11:20 +0530

    registery module

---
+++ setup.py
@@ -0,0 +1,41 @@
+import sys
+import setuptools
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+from registration import get_version
+
+
+setup(name="django-registration-redux",
+                 version=get_version().replace(' ', '-'),
+                 author='Andrew Cutler',
+                author_email='puneeth.prashant@gmail.com',
+                url='https://github.com/macropin/django-registration',
+                package_dir={'registration': 'registration'},
+                packages=find_packages(),
+
+                  include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],)
+
+
