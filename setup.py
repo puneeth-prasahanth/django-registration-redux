e9c26012c8ade1062567dcd889bdd6183295b0ca
Puneeth Prashanth Ganapuram <PuneethPrashanth.Ganapuram>
2019-02-04 16:23:59 +0530

    setup change

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
 import sys,os
 import setuptools
-#from registration import get_version
+import registration
+from registration import get_version
 from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
+##############
+#VERSION = (1, 1, 0, 'final', 0)
+#
+#
+#def get_version():
+#    "Returns a PEP 386-compliant version number from VERSION."
+#    assert len(VERSION) == 5
+#    assert VERSION[3] in ('alpha', 'beta', 'rc', 'final')
+#
+#    # Now build the two parts of the version number:
+#    # main = X.Y[.Z]
+#    # sub = .devN - for pre-alpha releases
+#    #     | {a|b|c}N - for alpha, beta and rc releases
+#
+#    parts = 2 if VERSION[2] == 0 else 3
+#    main = '.'.join(str(x) for x in VERSION[:parts])
+#
+#    sub = ''
+#    if VERSION[3] != 'final':
+#        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+#        sub = mapping[VERSION[3]] + str(VERSION[4])
+#
+#    return str(main + sub)
+#
 #############
-VERSION = (1, 1, 0, 'final', 0)
-
-
-def get_version():
-    "Returns a PEP 386-compliant version number from VERSION."
-    assert len(VERSION) == 5
-    assert VERSION[3] in ('alpha', 'beta', 'rc', 'final')
-
-    # Now build the two parts of the version number:
-    # main = X.Y[.Z]
-    # sub = .devN - for pre-alpha releases
-    #     | {a|b|c}N - for alpha, beta and rc releases
-
-    parts = 2 if VERSION[2] == 0 else 3
-    main = '.'.join(str(x) for x in VERSION[:parts])
-
-    sub = ''
-    if VERSION[3] != 'final':
-        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
-        sub = mapping[VERSION[3]] + str(VERSION[4])
-
-    return str(main + sub)
-
-############
 
 
 
